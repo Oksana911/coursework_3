2aabc821f5b78d606de0d678a4eab0a22ac2f115
Oksana911 <rosa111@mail.ru>
2022-07-19 16:57:15 +0700

    init

--- project/models.py
+++ project/models.py
@@ -1,9 +1,36 @@
-from sqlalchemy import Column, String
-
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from project.setup.db import models
 
 
 class Genre(models.Base):
     __tablename__ = 'genres'
-
     name = Column(String(100), unique=True, nullable=False)
+
+
+class Director(models.Base):
+    __tablename__ = 'directors'
+    name = Column(String(100), unique=True, nullable=False)
+
+
+class Movie(models.Base):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255))
+    description = Column(String(255))
+    trailer = Column(String(255))
+    year = Column(Integer)
+    rating = Column(Float)
+    genre_id = Column(Integer, ForeignKey("genre.id"))
+    director_id = Column(Integer, ForeignKey("director.id"))
+
+
+class User(models.Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String)
+    name = Column(String)
+    surname = Column(String)
+    favorite_genre = Column(String)
